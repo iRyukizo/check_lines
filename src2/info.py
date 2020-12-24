0ca581dfc2e541b454daaae89e925d6d1c268411
Hugo Moreau <hugo.moreau@epita.fr>
2020-12-24 04:06:20 +0100

    All updated

--- src2/info.py
+++ src2/info.py
@@ -1,4 +1,5 @@
 import os, sys
+from colorama import Fore, Style
 from . import functions
 
 class LinesInfos:
@@ -32,7 +33,7 @@ class LinesInfos:
         if (self._options[1] or self._options[2]):
             return functions.Functions.print_funcs(1 if self._options[1] else 2,
                     self._dictio_func[0], self._dictio_func[1])
-        return 0
+        return self.check()
 
     def print_info(self):
         """Debug purpose only"""
@@ -72,3 +73,107 @@ class LinesInfos:
         self._func = func
         self._max_len = maxlen
         self._dictio_func = dictio
+
+    def check(self):
+        """TODO: Docstring for check.
+
+        :returns: TODO
+
+        """
+        if self._options[0]:
+            print("-- remaining lines --")
+        res, file_ = 0, ""
+        for actu in self._func:
+            if (actu[1] == "function"):
+                f = open(actu[3])
+                if self._options[0] and file_ != actu[3]:
+                    file_ = actu[3]
+                    print("File:", Style.BRIGHT + Fore.CYAN + file_ + Style.RESET_ALL)
+                lines = f.readlines()
+                lines = [s.strip() for s in lines]
+                nb_lines = self.micro_check(lines, int(actu[2]))
+                if not self._options[0] and nb_lines > self._max_lines:
+                    res = 1
+                    self.print_err(actu, nb_lines, sys.stderr)
+                if self._options[0]:
+                    res |= self.remain(actu, nb_lines)
+                f.close()
+        return res
+
+    def micro_check(self, lines, start_func):
+        """TODO: Docstring for micro_check.
+
+        :lines: TODO
+        :start_func: TODO
+        :returns: TODO
+
+        """
+        braces, nb_lines, good = 0, 0, False
+        for k in lines[(start_func - 1):]:
+            if len(k) == 0 or k in self._ignore:
+                continue
+            if k == "{":
+                braces+=1
+                good = True
+                continue
+            if k == "}":
+                braces -= 1
+                continue
+            if good and braces <= 0:
+                break
+            if good:
+                nb_lines += 1
+        return nb_lines
+
+    def print_err(self, actu, nb_lines, f):
+        """TODO: Docstring for print_err.
+
+        :actu: TODO
+        :nb_lines: TODO
+        :f: TODO
+        :returns: TODO
+
+        """
+        print(Style.BRIGHT, end="", file=f)
+        print(actu[3]+":"+str(actu[2])+":"+str(actu[5])+":", end=" ", file=f)
+        print(Fore.RED + "warning:" + Fore.RESET, end=" ", file=f)
+        print("This function is too long: " + \
+                str(nb_lines) + " lines [expected " + \
+                str(self._max_lines) +" lines]", file=f)
+        print(Style.RESET_ALL, end="", file=f)
+        LinesInfos.strange_print(actu[4], actu[5], f)
+
+    def strange_print(proto, offset, f):
+        """TODO: Docstring for strange_print.
+
+        :proto: TODO
+        :offset: TODO
+        :f: TODO
+        :returns: TODO
+
+        """
+        print(proto, file=f)
+        print(Style.BRIGHT + Fore.GREEN, end="", file=f)
+        for i in range(offset):
+            print(" ", file=f, end="")
+        print("^", Style.RESET_ALL, file=f)
+
+    def remain(self, actu, nb_lines):
+        """
+        Will print remaining lines for each function.
+        actual : list of actual function
+        """
+        remaining_lines, res = self._max_lines - nb_lines, 0
+        print_lines = str(remaining_lines)
+        if remaining_lines < 0:
+            print_lines = Fore.RED + print_lines
+            res = 1
+        else:
+            print_lines = Fore.GREEN + print_lines
+        print("  {0} {1:<{5}} {2:>8}:\t{3:>15} {4}".format("Function:", \
+                Fore.BLUE + actu[0] + Fore.RESET, \
+                "(" + actu[2] + ":" + str(actu[5]) + ")", \
+                print_lines, \
+                "lines" + Fore.RESET, \
+                self._max_len + 10))
+        return res
